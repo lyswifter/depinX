f98a4e7d6cd57b0ca2eddde89ff3e98b3ab7ec5b
lyswifter <ly70835@163.com>
2024-05-22 14:55:20 +0800

    change checkview and reasoning view size

--- src/components/Why.vue
+++ src/components/Why.vue
@@ -39,23 +39,21 @@ let state = reactive({
     <div>
         <div class="why-view">
             <div class="mid-view">
-                <div style="width: 540px;margin: 0 auto;">
-                    <img src="../assets/ac_node@2x.png" width="540" height="100" alt="">
+                <div class="logo-view">
+                    <img src="../assets/ac_node@2x.png" width="100%" alt="">
                 </div>
                 <br>
-                <div style="width: 100%;margin: 0 auto;" class="text-1">WHY CHOOSE DePIN X ?</div>
+                <div class="text-1 text-1-f">WHY CHOOSE DePIN X ?</div>
                 <br>
                 <div class="feature-view">
                     <div v-for="(itemOut, i) in state.features" :key="i" class="row-view">
                         <div v-for="(itemIn, j) in itemOut" :key="j">
-                            <div class="item-card">
-                                <div style="width: 520px;height: 290px;margin: 0 auto;margin-top: 30px;"
+                            <div class="item-card card-size">
+                                <div style="width: 90%;height: 90%;margin: 0 auto;margin-top: 30px;"
                                     :class="itemIn.cls">
                                     <div><img :src="itemIn.dot" width="31" height="16" alt=""></div>
-                                    <br>
-                                    <div class="title-v">{{ itemIn.title }}</div>
-                                    <br>
-                                    <div class="content-v">{{ itemIn.content }}</div>
+                                    <div class="title-v title-v-f">{{ itemIn.title }}</div>
+                                    <div class="content-v content-v-f">{{ itemIn.content }}</div>
                                 </div>
                             </div>
                         </div>
@@ -72,7 +70,7 @@ let state = reactive({
                             —— Your best choice !
                         </div>
 
-                        <div style="display: flex;margin-top: 80px;">
+                        <div style="display: flex;flex-wrap: wrap;">
                             <div class="contact-v">
                                 <div class="text-4">Contact</div>
                                 <br>
@@ -80,16 +78,23 @@ let state = reactive({
                                     <img src="../assets/icon_wechat@2x.png" width="20" height="20" alt="">
                                     <div class="text-5">wxid 5bzocyoxb5hi21</div>
                                 </div>
+                                <br>
                                 <div style="display: flex;">
                                     <img src="../assets/icon_tg@2x.png" width="20" height="20" alt="">
                                     <div class="text-5">@DEPINX OFFICIAL</div>
                                 </div>
                             </div>
+
                             <div class="flex-grow" />
-                            <div><img src="../assets/code_wechat_checker_node@2x.png" width="151" height="151" alt="">
+
+                            <div class="qr-code-v">
+                                <div class="qr-code">
+                                    <img src="../assets/code_wechat_checker_node@2x.png" width="100%" height="100%" alt="">
+                                </div>
+                                <div class="qr-code" style="margin-left: 40px;">
+                                    <img src="../assets/code_tg_checker_node@2x.png" width="100%" height="100%" alt="">
+                                </div>
                             </div>
-                            <div style="margin-left: 40px;"><img src="../assets/code_tg_checker_node@2x.png" width="151"
-                                    height="151" alt=""></div>
                         </div>
                     </div>
                 </div>
@@ -105,16 +110,76 @@ let state = reactive({
     background: linear-gradient(180deg, #07195C 0%, #040C27 50%);
 }
 
-.mid-view {
-    width: 1200px;
-    margin: 0 auto;
+@media screen and (max-width: 767px) {
+    .mid-view {
+        width: 90%;
+        max-width: 1200px;
+        margin: 0 auto;
+    }
+
+    .logo-view {
+        width: 270px;
+        margin: 0 auto;
+    }
+
+    .text-1-f {
+        font-size: 44px;
+        line-height: 52px;
+    }
+
+    .card-size {
+        width: 335px;
+        height: 230px;
+    }
+
+    .title-v-f {
+        font-size: 20px;
+        line-height: 23px;
+    }
+
+    .content-v-f {
+        font-size: 14px;
+        line-height: 20px;
+    }
+}
+
+@media screen and (min-width: 768px) {
+    .mid-view {
+        width: 62.5%;
+        max-width: 1200px;
+        margin: 0 auto;
+    }
+
+    .logo-view {
+        width: 540px;
+        margin: 0 auto;
+    }
+
+    .text-1-f {
+        font-size: 94px;
+        line-height: 110px;
+    }
+
+    .card-size {
+        width: 580px;
+        height: 350px;
+    }
+
+    .title-v-f {
+        font-size: 30px;
+        line-height: 35px;
+    }
+
+    .content-v-f {
+        font-size: 18px;
+        line-height: 25px;
+    }
 }
 
 .text-1 {
-    height: 115px;
+    width: 100%;
+    margin: 0 auto;
     font-weight: 200;
-    font-size: 94px;
-    line-height: 110px;
     text-align: center;
     font-style: normal;
     text-transform: none;
@@ -124,29 +189,23 @@ let state = reactive({
 }
 
 .feature-view {
-    width: 1200px;
+    width: 100%;
     margin: 0 auto;
 }
 
 .row-view {
     display: flex;
-    justify-content: space-between
+    flex-wrap: wrap;
+    justify-content: center;
 }
 
 .item-card {
     margin-top: 30px;
-    width: 580px;
-    height: 350px;
     border-radius: 30px 30px 30px 30px;
     border: 1px solid rgba(63, 102, 242, 0.3);
 }
 
 .item-card:hover {
-    /* border-radius: 30px 30px 30px 30px;
-    background: rgba(6, 15, 50, 0.54);
-    border: 1px solid transparent;
-    background-clip: padding-box;
-    border-image: linear-gradient(90deg, rgba(27, 77, 255, 1), rgba(1, 255, 255, 1)) 2 2; */
     border-color: rgba(27, 77, 255, 1);
 }
 
@@ -167,33 +226,29 @@ let state = reactive({
 }
 
 .title-v {
-    width: 450px;
-    height: 74px;
     font-weight: 600;
-    font-size: 30px;
     color: #FFFFFF;
-    line-height: 35px;
     text-align: left;
     font-style: normal;
     text-transform: none;
 }
 
 .content-v {
-    width: 202px;
+    width: 60%;
     font-weight: 400;
-    font-size: 18px;
     color: #B7C5F6;
-    line-height: 25px;
     text-align: left;
     font-style: normal;
     text-transform: none;
 }
 
+/* ----- */
+
 .checknode-view {
-    width: 1200px;
+    width: 100%;
+    max-width: 1200px;
     margin: 0 auto;
     margin-top: 40px;
-    height: 437px;
     border-radius: 30px;
     border: 1px solid rgba(63, 102, 242, 0.3);
     background: no-repeat center/100% url(../assets/acnode_best_choice@2x.png);
@@ -204,8 +259,6 @@ let state = reactive({
 }
 
 .text-2 {
-    width: 1120px;
-    height: 59px;
     font-weight: 600;
     font-size: 48px;
     color: #FFFFFF;
@@ -216,8 +269,6 @@ let state = reactive({
 }
 
 .text-3 {
-    width: 1120px;
-    height: 37px;
     font-weight: 400;
     font-size: 30px;
     color: #FFFFFF;
@@ -252,13 +303,40 @@ let state = reactive({
 }
 
 .contact-v {
-    width: 521px;
     padding-top: 40px;
+    margin-top: 80px;
     border-top: 1px solid;
     border-image: linear-gradient(90deg, rgba(27, 77, 255, 1), rgba(1, 255, 255, 1)) 1 1;
 }
 
 .flex-grow {
     flex-grow: 1;
+}
+
+.qr-code-v {
+    display: flex;
+    margin-top: 40px;
+}
+
+@media screen and (max-width: 767px) {
+    .qr-code {
+        width: 137px;
+        height: 137px;
+    }
+
+    .contact-v {
+        width: 100%;
+    }
+
+    .checknode-view {
+        background: no-repeat center/100% url(../assets/acnode_best_choice_mobile@2x.png);
+    }
+}
+
+@media screen and (min-width: 768px) {
+    .qr-code {
+        width: 151px;
+        height: 151px;
+    }
 }
 </style>
